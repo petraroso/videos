c78a1bb6a770c305771b65d96b84f4a9cd1ba5d3
petraroso <proso00@fesb.hr>
2021-11-16 16:52:08 +0100

    Setting up Youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar/index";
+import youtube from "../apis/youtube"
 import styles from "./App.module.css";
 
 class App extends React.Component {
+  onInputSubmit=(input)=>{
+    youtube.get('/search',{
+      params: {
+        q: input
+      }
+    })
+  }
   render() {
     return (
       <div className={styles.container}>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onInputSubmit}/>
       </div>
     );
   }
