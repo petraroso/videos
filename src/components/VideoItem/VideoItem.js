ee7c7a228eab7fb4ecfaaf78494db6ece51a16cf
petraroso <proso00@fesb.hr>
2021-11-20 19:54:34 +0100

    Video items css styling

--- src/components/VideoItem/VideoItem.js
+++ src/components/VideoItem/VideoItem.js
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from "./style.module.css"
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
+    <div className={styles.videoItem}>
       <img src={video.snippet.thumbnails.medium.url}></img>
       <div>{video.snippet.title}</div>
     </div>
