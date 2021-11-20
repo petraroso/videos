f1b37508b4747748a259d1ee64fe3f70021a5d67
petraroso <proso00@fesb.hr>
2021-11-20 14:07:30 +0100

    Fixing yt api key name

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-require("dotenv").config();
 
-const KEY = process.env.API_KEY;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
-  params: { part: "snippet", maxResults: 5, key: KEY },
 });
--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,33 @@
 import React from "react";
 import SearchBar from "./SearchBar/index";
-import youtube from "../apis/youtube"
+import youtube from "../apis/youtube";
 import styles from "./App.module.css";
+require("dotenv").config();
+
+const KEY = process.env.REACT_APP_API_KEY_YOUTUBE;
 
 class App extends React.Component {
-  onInputSubmit=(input)=>{
-    youtube.get('/search',{
+  state = { videos: [] };
+
+  onInputSubmit = async (input) => {
+    const response = await youtube.get("/search", {
       params: {
-        q: input
-      }
-    })
-  }
+        credentials: "include",
+        q: input,
+        part: "snippet",
+        maxResults: 5,
+        key: KEY,
+      },
+    });
+    console.log(response.data.items);
+
+    this.setState({ videos: response.data.items });
+  };
   render() {
     return (
       <div className={styles.container}>
-        <SearchBar onFormSubmit={this.onInputSubmit}/>
+        <SearchBar onFormSubmit={this.onInputSubmit} />
+        Showing {this.state.videos.length} videos
       </div>
     );
   }
